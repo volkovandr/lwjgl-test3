20d356e4c9d6221efde37f41f0a67d883a99fa27
volkovandr <andrey.v.volkov@gmail.com>
2020-03-15 13:04:24 +0100

    Changed colors to make it more ... beautiful

--- src/main/scala/org/sample/logic/House.scala
+++ src/main/scala/org/sample/logic/House.scala
@@ -15,23 +15,23 @@ class House extends GameObject {
         0.5f, 0.5f,   -0.5f,
 
         //roof
-        0.0f, 0.7f,   0.5f,
-        0.0f, 0.7f,   -0.5f
+        0.0f, 0.8f,   0.5f,
+        0.0f, 0.8f,   -0.5f
     )
 
     private val colors = Array(
         1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
 
-        0.8f, 0.0f, 0.0f,
-        0.0f, 0.8f, 0.0f,
-        0.0f, 0.8f, 0.0f,
-        0.8f, 0.0f, 0.0f,
+        0.8f, 0.0f, 8.0f,
+        0.0f, 0.8f, 8.0f,
+        0.0f, 0.8f, 8.0f,
+        0.8f, 0.0f, 8.0f,
 
-        0.0f, 0.0f, 1.0f,
-        0.0f, 0.0f, 0.8f
+        0.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 0.8f
     )
 
     private val indices = Array(
