7a5d79733b84f75a18423edb0f352012d8296014
volkovandr <andrey.v.volkov@gmail.com>
2020-03-15 23:34:12 +0100

    Fixed texture loading

--- src/main/scala/org/sample/graph/Texture.scala
+++ src/main/scala/org/sample/graph/Texture.scala
@@ -13,12 +13,15 @@ import org.lwjgl.opengl.GL11.glBindTexture
 import org.lwjgl.opengl.GL11.glPixelStorei
 import org.lwjgl.opengl.GL11.glTexParameteri
 import org.lwjgl.opengl.GL11.glTexImage2D
+import org.lwjgl.opengl.GL11.glGetTexLevelParameteriv
 import org.lwjgl.opengl.GL30.glGenerateMipmap
 import org.lwjgl.stb.STBImage.stbi_load
 import org.lwjgl.stb.STBImage.stbi_failure_reason
 import org.lwjgl.stb.STBImage.stbi_image_free
 
 import org.lwjgl.opengl.GL11.GL_TEXTURE_2D
+import org.lwjgl.opengl.GL11.GL_TEXTURE_WIDTH
+import org.lwjgl.opengl.GL11.GL_TEXTURE_HEIGHT
 // import org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER
 // import org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER
 // import org.lwjgl.opengl.GL11.GL_NEAREST
@@ -54,12 +57,19 @@ class Texture {
         val textureId = glGenTextures()
         glBindTexture(GL_TEXTURE_2D, textureId)
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer)
         // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         glGenerateMipmap(GL_TEXTURE_2D)
         texture = Some(textureId)
-        println(s"Loaded texture from [$resourceName] ${decoder.getWidth()}x${decoder.getHeight()} ID=$textureId")
+        Using(MemoryStack.stackPush()) { stack =>
+            val pWidth = stack.ints(1)
+            val pHeight = stack.ints(1)
+            val mipLevel = 0
+            glGetTexLevelParameteriv(GL_TEXTURE_2D, mipLevel, GL_TEXTURE_WIDTH, pWidth)
+            glGetTexLevelParameteriv(GL_TEXTURE_2D, mipLevel, GL_TEXTURE_WIDTH, pHeight)
+            println(s"Loaded texture from [$resourceName] ${pWidth.get()}x${pHeight.get()} ID=$textureId")
+        }
     }
 
     def cleanup(): Unit = texture.foreach(id => glDeleteTextures(id))
